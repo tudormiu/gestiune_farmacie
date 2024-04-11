ce84ae9a25540890b811b646d07cdc4f756ce0b5
Tudor <miutudorgabriel@gmail.com>
2024-04-11 16:40:48 +0300

    fixes push

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         ArrayList<Medicament> medicamente = new ArrayList<>();
         ArrayList<Angajat> angajati = new ArrayList<>();
         ArrayList<Furnizor> furnizori = new ArrayList<>();
